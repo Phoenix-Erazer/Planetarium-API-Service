2d5a90e0d19421aa7c3e040b832a5d2332f15fd6
Andii Rashevskyi <phoenixerazer@gmail.com>
2023-10-08 14:29:50 +0300

    fix name project to planetarium_api_service

--- planetarium_api_service/asgi.py
+++ planetarium_api_service/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Planetarium_API_Service project.
+ASGI config for planetarium_api_service project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Planetarium_API_Service.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'planetarium_api_service.settings')
 
 application = get_asgi_application()
