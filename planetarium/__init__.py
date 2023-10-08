bc76a87f75c480a6bbd9a057e4a27094c1aa17e8
Andii Rashevskyi <phoenixerazer@gmail.com>
2023-10-08 20:21:27 +0300

    add all models in project

---
+++ planetarium/__init__.py
@@ -0,0 +1,2 @@
+class PlanetariumDome:
+    pass
--- planetarium/models.py
+++ planetarium/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class ShowTheme(models.Model):
+    name = models.CharField(max_length=64, null=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class AstronomyShow(models.Model):
+    title = models.CharField(max_length=64, null=True)
+    description = models.ManyToManyField(ShowTheme, related_name="astronomy_shows")
+
+    def __str__(self):
+        return str(self.title)
+
+
+class PlanetariumDome(models.Model):
+    name = models.CharField(max_length=255, null=True)
+    rows = models.IntegerField(default=0)
+    seats_in_row = models.IntegerField()
+
+
+class Reservation:
+    created_at = models.DateTimeField()
+    # user
+
+
+class ShowSession(models.Model):
+    astronomy_show = models.ForeignKey(PlanetariumDome, on_delete=models.CASCADE)
+    planetarium_dome = models.ForeignKey(AstronomyShow, on_delete=models.CASCADE)
+    show_time = models.DateTimeField()
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    show_sessions = models.ForeignKey(AstronomyShow, on_delete=models.CASCADE)
+    reservations = models.ForeignKey(Reservation, on_delete=models.CASCADE)
