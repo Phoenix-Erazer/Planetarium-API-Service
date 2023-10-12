8d97e98a0801f538fe2d436a93fbbbe7a30053af
Andii Rashevskyi <phoenixerazer@gmail.com>
2023-10-12 23:29:09 +0300

    Add in class Ticket def(clean) and def(save)

--- planetarium/models.py
+++ planetarium/models.py
@@ -1,5 +1,8 @@
 from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.db.models import UniqueConstraint
+
 from user.models import User
 
 
@@ -34,7 +37,7 @@ class Reservation(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f" {self.user}: {self.created_at}"
+        return f"{self.created_at}"
 
 
 class ShowSession(models.Model):
@@ -51,5 +54,25 @@ class ShowSession(models.Model):
 class Ticket(models.Model):
     row = models.IntegerField()
     seat = models.IntegerField()
-    show_sessions = models.ForeignKey(ShowSession, on_delete=models.CASCADE)
-    reservations = models.ForeignKey(Reservation, on_delete=models.CASCADE)
+    show_sessions = models.ForeignKey(ShowSession, on_delete=models.CASCADE, related_name="tickets")
+    reservations = models.ForeignKey(Reservation, on_delete=models.CASCADE, related_name="tickets")
+
+    def clean(self):
+        if not(1 <= self.seat <= self.show_sessions.planetarium_dome.seats_in_row):
+            raise ValidationError({
+                "seat": f"seat must be in range [1, {self.show_sessions.planetarium_dome.seats_in_row}, not {self.seat}"
+            })
+
+        if not(1 <= self.row <= self.show_sessions.planetarium_dome.rows):
+            raise ValidationError({
+                "rows": f"row must be in range [1, {self.show_sessions.planetarium_dome.rows}, not {self.row}"
+            })
+
+    def save(
+        self, force_insert=False, force_update=False, using=None, update_fields=None
+    ):
+        self.full_clean()
+        return super(Ticket, self).save(force_insert, force_update, using, update_fields)
+
+    class Meta:
+        unique_together = ("row", "seat", "show_sessions")
